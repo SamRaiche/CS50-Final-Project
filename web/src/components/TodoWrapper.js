15e6e0f404b244c5abd7ff83181f6fc6af0d1668
Sam Raiche <raichesam09@gmail.com>
2024-09-01 17:20:14 -0500

    minor changes

--- web/src/components/TodoWrapper.js
+++ web/src/components/TodoWrapper.js
@@ -8,7 +8,7 @@ export const TodoWrapper = () => {
     const [todo, setTodo] = useState([])
 
     const updateTask = tassk => {
-        setTodo([...todo, {id: uuidv4(), task: tassk}]);
+        setTodo([...todo, {id: uuidv4(),task: tassk, completed: false, editing: false}]);
         console.log(todo);
     }
 
