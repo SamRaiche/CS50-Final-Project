26be0d0e3ad5c80b223a65d6bbea89685fcc2014
Sam Raiche <raichesam09@gmail.com>
2024-09-01 11:40:20 -0500

    Big changes! Keeping state when adding tasks to list. Also, updated CSS

--- web/src/components/TodoForm.js
+++ web/src/components/TodoForm.js
@@ -1,10 +1,24 @@
-import React from 'react'
+import React, {useState} from 'react';
 
-export const TodoForm = () => {
+    export const TodoForm = () => {
+        const [input, setInput] = useState(""); // Initialize state
+    
+        const addTask = (event) => {
+            event.preventDefault(); // Prevent the form from reloading
+            console.log(input); // Log the input value
+            setInput(""); // Optionally clear the input field
+        };
+
     return (
-        <form className='todo-css'>
-            <input type='text' className='input' placeholder='What is the task?'></input>
-            <button className='button' type='submit'>Add</button>
+        <form className='todo-css' onSubmit={addTask}>
+            <input 
+                type='text' 
+                className='input' 
+                placeholder='What is the task?' 
+                onChange = {(e) => setInput(e.target.value)}>
+            </input>
+
+            <button className='button' type='submit'>Add Task</button>
         </form>
-    )
+    );
 }
